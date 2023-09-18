dfc38d1ff6d71e3ab16715a1c7a9cf5fe89cc93b
Fantagirll <138145420+Fantagirll@users.noreply.github.com>
2023-09-19 00:34:31 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,21 +21,21 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
+global.owner = ['919233943441'] //ur owner number
+global.ownernomer = "919233943441" //ur owner number2
+global.ownername = "Fanta girl" //ur owner name
+global.ytname = "YT: Fanta" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
+global.botname = "Fanta Bot MD"
+global.ownernumber = '919233943441'
+global.ownername = 'Fanta girl'
+global.ownerNumber = ["919233943441@s.whatsapp.net"]
 global.ownerweb = "https://youtube.com/@DGXeon"
 global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
+global.wagc = "https://chat.whatsapp.com/FAg85qE2VT18ALetUoRWkY"
 global.themeemoji = '🪀'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
